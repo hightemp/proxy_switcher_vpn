bd8a486a56f90cdfb5048292753d1e17087fedc9
hightemp <hightemp.unknown@gmail.com>
2026-06-26 20:18:58 +0300

    chore: bump version to 0.1.0 (versionCode=100)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,8 @@ android {
         applicationId = "com.hightemp.proxy_switcher_vpn"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 100
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
